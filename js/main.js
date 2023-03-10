cbd8b4d78158833f68009163b3f309964589a222
samir98441 <samir98441@gmail.com>
2023-03-10 16:41:51 +0545

    added to git

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".three").owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: false
+    });
+});
